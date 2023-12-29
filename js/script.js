e1956237beeb82c805ebdddb610e598a1c0c2f3d
munaff-adahan <tuwanmunaff@gmail.com>
2023-12-29 10:08:57 +0530

    Second Commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 let list = document.querySelectorAll(".navigation li");
 
-        function activeLink() {
-            list.forEach((item) => item.classList.remove('hovered'));
-            this.classList.add('hovered');
-        }
-        list.forEach((item) => item.addEventListener("mouseover", activeLink));
+function activeLink() {
+  list.forEach((item) => item.classList.remove("hovered"));
+  this.classList.add("hovered");
+}
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
 
+//Toggle Menu//
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+let main = document.querySelector(".main");
 
-        //Toggle Menu//
-        let toggle = document.querySelector('.toggle');
-        let navigation = document.querySelector('.navigation');
-        let main = document.querySelector('.main');
-
-        toggle.onclick = function(){
-            navigation.classList.toggle('active')
-            main.classList.toggle('active')
-        }
-        
+toggle.onclick = function () {
+  navigation.classList.toggle("active");
+  main.classList.toggle("active");
+};
